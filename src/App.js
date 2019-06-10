ea1417c02c363c5057a26297a68a157375107903
Pavel Klochkov <paul.klochkov@gmail.com>
2019-06-10 17:05:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost'
+import gql from 'graphql-tag';
+import { ApolloProvider, Query } from 'react-apollo'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Query query={
+        gql`
+          {
+            recipes {
+              id
+              title
+            }
+          }
+        
+          `
+      }
+      >
+        {({ data, loading, error }) => {
+          if (loading) return <p>Loading ...</p>
+          if (error) return <p>Something went wrong ...</p>
+
+          return (
+            <ul>
+              {data.recipes.map(({ id, title }) =>
+                <li key={id}>{title}</li>
+              )}
+            </ul>
+          )
+        }}
+      </Query>
+
+    </ApolloProvider>
   );
 }
 
