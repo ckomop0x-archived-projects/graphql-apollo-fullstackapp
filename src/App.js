9d2c790a941bf6e93d9d966a990251f6766e411b
Pavel Klochkov <paul.klochkov@gmail.com>
2019-06-10 18:00:14 +0200

    Old modified files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost'
-import gql from 'graphql-tag';
-import { ApolloProvider, Query } from 'react-apollo'
+import { ApolloProvider } from 'react-apollo'
+import Recipes from './Recipes'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000'
@@ -10,32 +10,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Query query={
-        gql`
-          {
-            recipes {
-              id
-              title
-            }
-          }
-        
-          `
-      }
-      >
-        {({ data, loading, error }) => {
-          if (loading) return <p>Loading ...</p>
-          if (error) return <p>Something went wrong ...</p>
-
-          return (
-            <ul>
-              {data.recipes.map(({ id, title }) =>
-                <li key={id}>{title}</li>
-              )}
-            </ul>
-          )
-        }}
-      </Query>
-
+      <Recipes />
     </ApolloProvider>
   );
 }
